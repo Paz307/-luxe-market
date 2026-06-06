e074abf8be929f93ae7cd95363b7058c86938ddf
Paz307 <benimanapatrick50@gmail.com>
2026-06-06 22:12:23 +0200

    fix PORT for Railway

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(flash());
 const routes = require('./routes/auth');
 app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`\n✦ Luxe Market running at http://localhost:${PORT}\n`);
 });
